e2012ab1a4a34d3cfefcf1e8ead9af0561123dd4
Ricardo Leyva <ricardomtzleyva@hotmail.com>
2024-07-29 22:20:15 -0400

    Added GH Pages and its config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/https://github.com/ricardoleyva/conference_event_planner.git",
+  base: "/conference_event_planner",
   plugins: [react()],
 })
