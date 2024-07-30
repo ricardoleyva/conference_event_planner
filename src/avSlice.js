9b45f0c03f91b29eab6deb52bb6889102296b419
Ricardo Leyva <ricardomtzleyva@hotmail.com>
2024-07-29 23:04:30 -0400

    Added img

--- src/avSlice.js
+++ src/avSlice.js
@@ -4,32 +4,32 @@ export const avSlice = createSlice({
   name: "av",
   initialState: [
       {
-        img: "./public/projector.jpg",
+        img: "https://pixabay.com/photos/business-computer-conference-20031/",
         name: "Projectors",
         cost: 200,
         quantity: 0,
     },
     {
-        img: "./public/speakers.jpg",
+        img: "https://pixabay.com/photos/speakers-bluetooth-tech-speaker-4109274/",
         name: "Speaker",
         cost: 35,
         quantity: 0,
     },
     {
-        img: "./public/microphone.jpg",
+        img: "https://pixabay.com/photos/public-speaking-mic-microphone-3926344/",
         name: "Microphones",
         cost: 45,
         quantity: 0,
     },
     {
-        img: "./public/whiteboard.jpg",
+        img: "https://pixabay.com/photos/whiteboard-dry-erase-marker-blank-2903269/",
         name: "Whiteboards",
         cost: 80,
         quantity: 0,
     },
 
     {
-        img: "./public/signs.jpg",
+        img: "https://pixabay.com/photos/signpost-waypoint-wood-grain-board-235079/",
         name: "Signage",
         cost: 80,
         quantity: 0,
