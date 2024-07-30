58819c272ce86e777d67848792f68a9d6ec79e3d
Ricardo Leyva <ricardomtzleyva@hotmail.com>
2024-07-29 22:05:56 -0400

    Modify 7 files, added 10 img

--- src/avSlice.js
+++ src/avSlice.js
@@ -3,16 +3,52 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
+      {
+        img: "./public/projector.jpg",
+        name: "Projectors",
+        cost: 200,
+        quantity: 0,
+    },
+    {
+        img: "./public/speakers.jpg",
+        name: "Speaker",
+        cost: 35,
+        quantity: 0,
+    },
+    {
+        img: "./public/microphone.jpg",
+        name: "Microphones",
+        cost: 45,
+        quantity: 0,
+    },
+    {
+        img: "./public/whiteboard.jpg",
+        name: "Whiteboards",
+        cost: 80,
+        quantity: 0,
+    },
+
+    {
+        img: "./public/signs.jpg",
+        name: "Signage",
+        cost: 80,
+        quantity: 0,
+    },
   ],
 
 
   reducers: {
     incrementAvQuantity: (state, action) => {
-      
+      const item = state[action.payload];
+      if (item) {
+          item.quantity++;
+      }
     },
     decrementAvQuantity: (state, action) => {
-     
+      const item = state[action.payload];
+      if (item && item.quantity > 0) {
+          item.quantity--;
+      }
     },
   },
 });
