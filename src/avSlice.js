5fafcd2aac78b44b19abc7cab26b31be93cb85c8
Ricardo Leyva <ricardomtzleyva@hotmail.com>
2024-07-29 23:14:30 -0400

    Added img from source pixabay.com

--- src/avSlice.js
+++ src/avSlice.js
@@ -4,32 +4,32 @@ export const avSlice = createSlice({
   name: "av",
   initialState: [
       {
-        img: "https://pixabay.com/photos/business-computer-conference-20031/",
+        img: "https://pixabay.com/get/g14d583419cda5dd4a4b2dde17fa7440a3a9f7679943af1c9ae9b4fe942b4e7386e7d7f6c5593c2b1bd660ace05dc49cb_1280.jpg",
         name: "Projectors",
         cost: 200,
         quantity: 0,
     },
     {
-        img: "https://pixabay.com/photos/speakers-bluetooth-tech-speaker-4109274/",
+        img: "https://pixabay.com/get/g39588aa9f328ea385018ba4383fc1e406f80353fa308f126e376cb4b1cdacfb051c7c64a089cff38a50a1412ab5b50010f4d4b8ad2fb6bbdd6b71c3bb2659270_1280.jpg",
         name: "Speaker",
         cost: 35,
         quantity: 0,
     },
     {
-        img: "https://pixabay.com/photos/public-speaking-mic-microphone-3926344/",
+        img: "https://pixabay.com/get/ge3be0293cc51258afa66b65b74c65627cb9c4b784ee3e8e88a4b87859bc358329ca09ddab98f01f05a4461fec1487278b57fa986bab6ab1390b6e85b7df08337_1280.jpg",
         name: "Microphones",
         cost: 45,
         quantity: 0,
     },
     {
-        img: "https://pixabay.com/photos/whiteboard-dry-erase-marker-blank-2903269/",
+        img: "https://pixabay.com/get/g43f4cba5c92caaaaaecbf1d2a65deb1e8999e80e78de4ada15467b854e086a338fbd897a7a3f7a09bd61d9499965e2077f0b0cbe3056110f07d568a6c2ae3a87_1280.png",
         name: "Whiteboards",
         cost: 80,
         quantity: 0,
     },
 
     {
-        img: "https://pixabay.com/photos/signpost-waypoint-wood-grain-board-235079/",
+        img: "https://pixabay.com/get/gd91ff148eea28a7aaf9d6ef4335a630cb97b425b1298a199bb68dea408e7fe8125156e52b07912af66ba874257a3268b_1280.jpg",
         name: "Signage",
         cost: 80,
         quantity: 0,
--- src/venueSlice.js
+++ src/venueSlice.js
@@ -5,31 +5,31 @@ export const venueSlice = createSlice({
   name: "venue",
   initialState: [
     {
-      img: "https://pixabay.com/photos/chairs-empty-office-room-table-2181916/",
+      img: "https://pixabay.com/get/g9a5c6d7187957a02049a1b563496f4555f30362d99cc2464e8cf4dade847325a6a40cf6b8a45f6e81d0e71d8dba76bd5_1280.jpg",
       name: "Conference Room (Capacity:15)",
       cost: 3500,
       quantity: 0,
     },
     {
-      img: "https://pixabay.com/photos/event-venue-auditorium-meeting-1597531/",
+      img: "https://pixabay.com/get/g511e36e47a6c9afbbbaf73f282cedd737a714c26af576d337ba429f8868b67cd6e040913496bdb56f548e611ca5a933bedac5ca7acc1fefec06437bf12f0047a_1280.jpg",
       name: "Auditorium Hall (Capacity:200)",
       cost: 5500,
       quantity: 0,
     },
     {
-      img: "https://pixabay.com/photos/convention-center-chair-seminar-3908238/",
+      img: "https://pixabay.com/get/g1092788b74ccd210065daf3ad6117e383b5f0255392ebc76deb0408219523ee986fe7281b40f2f4c10b97bd9495bf44e6fc8dc6f82f6a638d8bfbad289184d50_1280.jpg",
       name: "Presentation Room (Capacity:50)",
       cost: 700,
       quantity: 0,
     },
     {
-      img: "https://pixabay.com/photos/chairs-empty-office-room-table-2181916/",
+      img: "https://pixabay.com/get/gf21e6c7e24d04502279ae88de52390bd2f0382b4178b8be4a8be0b113ba9e0a9ebbb7c4072e7b3909302cce7e032cb175955dff25eb22c3fc2aec9cb2688176d_1280.jpg",
       name: "Large Meeting Room (Capacity:10)",
       cost: 900,
       quantity: 0,
     },
     {
-      img: "https://pixabay.com/photos/laptops-meeting-businessmen-593296/",
+      img: "https://pixabay.com/get/g1ab0da020ba9c28989178517114c12d76fc843628cf818b253ce820c069fd2657ba3c79529b42a2b5579d883be4d6a6f_1280.jpg",
       name: "Small Meeting Room (Capacity:5)",
       cost: 1100,
       quantity: 0,
